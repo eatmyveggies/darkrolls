8ed2ffd6bc4be702d1f9e6cc3bef77437ed2644b
Radu Mutilica <radu@mutilica.com>
2018-02-27 20:35:08 +0000

    Fixed issue with multiplier going below 1 on first roll of dubs - ugly quick fix, to be properly fixed when/if rng mechanism is overhauled

--- sharkling/roll.py
+++ sharkling/roll.py
@@ -132,6 +132,7 @@ class Dubs(__Roll):
         if rng <= min((streak_multiplier - 1) * Dubs.BAD_RNG_BASE_PERCENT, 50):
             self.rng_points_multiplier = Dubs.BAD_RNG_POINT_MULTIPLIER
             self.streak_multiplier += Dubs.BAD_RNG_MULTIPLIER_MODIFIER
+
             # hacky way to make sure the multiplier doesn't go below 1
             self.streak_multiplier = max(self.streak_multiplier, 1)
             raise BadRng(roll=self)
